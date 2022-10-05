85b31561142f43ccf012020162c25f17d1b5729c
marcosolime <marco.solime@gmx.com>
2022-10-05 13:48:03 +0200

    added salud feature

--- einstein.py
+++ einstein.py
@@ -2,4 +2,9 @@ def compute(aloha):
     if type(aloha) == str:
         return aloha + "!"
 
+def salud():
+    name = input('Put here your name: ')
+    return str.capitalize(name)
+
 print(compute(3))
+print(salud())
